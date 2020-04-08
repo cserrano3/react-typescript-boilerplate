c8f54df1dc05b4200f296f504a0a854bb73f4020
Carolina Fernandes Serrano Lopes <CIANDT\carolinasl@lnb024608bhz.ciandt.global>
2020-04-08 11:18:59 -0300

    include command to fix all lint errors and warnings automatically

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { Hello } from './components/Hello';
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { App } from './App';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+ReactDOM.render(<App />,
+    document.getElementById('root')
 );
